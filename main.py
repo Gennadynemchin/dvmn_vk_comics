64ff96d4a95d2e0524020646819a01dc6b3fd85f
gennadynemchin@gmail.com <gennadynemchin@gmail.com>
2022-10-09 22:40:17 +0300

    Added comments for get_comicbook func;

--- main.py
+++ main.py
@@ -4,21 +4,21 @@ from pathlib import Path
 from urllib.parse import urlsplit
 
 
-def get_comics(number_of_comics, folder):
-    Path(folder).mkdir(parents=True, exist_ok=True)
+def get_comicbook(number_of_comics, folder='Files'):
+    Path(folder).mkdir(parents=True, exist_ok=True)  # settings for comics directory creating
     url = f'https://xkcd.com/{number_of_comics}/info.0.json'
-    response = requests.get(url)
+    response = requests.get(url)  # get json from xkcd.com
     response.raise_for_status()
-    decoded_response = response.json()
-    url_to_download_comics = decoded_response['img']
-    response_comics = requests.get(url_to_download_comics)
-    response_comics.raise_for_status()
-    title = urlsplit(url_to_download_comics).path
-    filename = os.path.basename(title)
-    comics_comment = decoded_response['alt']
-    with open(f'{folder}/{filename}', 'wb') as file:
-        file.write(response_comics.content)
-    return comics_comment
+    url_to_download_comicbook = response.json()['img']  # get comics url from xkcd.com json
+    comicbook_comment = response.json()['alt']  # get comics comment from json
+    response_comicbook = requests.get(url_to_download_comicbook)  # request a path for download a comics
+    response_comicbook.raise_for_status()
+    comicbook_title = urlsplit(url_to_download_comicbook).path  # get current comics title
+    filename = os.path.basename(comicbook_title)  # creating filename from comics url
+
+    with open(f'{folder}/{filename}', 'wb') as file:  # writing the comics to directory
+        file.write(response_comicbook.content)
+    return comicbook_comment
 
 
-print(get_comics(353, 'Files'))
+get_comicbook(353)
