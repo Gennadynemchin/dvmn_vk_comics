936aa216f03b363d15849099c516cf1ded7c2146
gennadynemchin@gmail.com <gennadynemchin@gmail.com>
2022-10-09 21:58:29 +0300

    Created get_comics() func;

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import requests
 import os
-import posixpath
-import urllib.parse
 from pathlib import Path
+from urllib.parse import urlsplit
 
 
 def get_comics(number_of_comics, folder):
     Path(folder).mkdir(parents=True, exist_ok=True)
-    filename = 'python_comics.png'
     url = f'https://xkcd.com/{number_of_comics}/info.0.json'
     response = requests.get(url)
     response.raise_for_status()
@@ -15,7 +13,8 @@ def get_comics(number_of_comics, folder):
     url_to_download_comics = decoded_response['img']
     response_comics = requests.get(url_to_download_comics)
     response_comics.raise_for_status()
-    filename = os.path.basename(filename)
+    title = urlsplit(url_to_download_comics).path
+    filename = os.path.basename(title)
     comics_comment = decoded_response['alt']
     with open(f'{folder}/{filename}', 'wb') as file:
         file.write(response_comics.content)
