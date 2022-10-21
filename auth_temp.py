2c77ba97866b5ddd97f8c1164b50fc578ea84341
gennadynemchin@gmail.com <gennadynemchin@gmail.com>
2022-10-22 00:10:15 +0300

    Added temp files;

--- auth_temp.py
+++
@@ -1,9 +0,0 @@
-import webbrowser
-
-
-webbrowser.open('https://oauth.vk.com/authorize?'
-                'client_id=51447720&'
-                'display=mobile&'
-                'scope=photos,groups,wall&'
-                'response_type=token&'
-                'v=5.131')
