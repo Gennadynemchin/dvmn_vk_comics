8263d930b7999ced61373aa374375446ad7696c2
gennadynemchin@gmail.com <gennadynemchin@gmail.com>
2022-10-22 13:03:19 +0300

    Added save_comicbook() func;

--- xkcd.py
+++ xkcd.py
@@ -6,20 +6,29 @@ from urllib.parse import urlsplit
 
 
 def get_comicbook(number_of_comics, folder):
-    Path(folder).mkdir(parents=True, exist_ok=True)  # settings for comics directory creating
+    # Path(folder).mkdir(parents=True, exist_ok=True)
     url = f'https://xkcd.com/{number_of_comics}/info.0.json'
-    response = requests.get(url)  # get json from xkcd.com
+    response = requests.get(url)
     response.raise_for_status()
-    url_to_download_comicbook = response.json()['img']  # get comics url from xkcd.com json
-    comicbook_comment = response.json()['alt']  # get comics comment from json
-    response_comicbook = requests.get(url_to_download_comicbook)  # request a path for download a comics
-    response_comicbook.raise_for_status()
-    comicbook_title = urlsplit(url_to_download_comicbook).path  # get current comics title
-    filename = os.path.basename(comicbook_title)  # creating filename from comics url
+    url_to_download_comicbook = response.json()['img']
+    comicbook_comment = response.json()['alt']
+    # response_comicbook = requests.get(url_to_download_comicbook)
+    # response_comicbook.raise_for_status()
+    # with open(f'{folder}/{filename}', 'wb') as file:
+        # file.write(response_comicbook.content)
+    return {'comicbook_comment': comicbook_comment,
+            'download_url': url_to_download_comicbook}
 
-    with open(f'{folder}/{filename}', 'wb') as file:  # writing the comics to directory
-        file.write(response_comicbook.content)
-    return {'comicbook_comment': comicbook_comment, 'filename': filename}
+
+def save_comicbook(download_url, folder):
+    Path(folder).mkdir(parents=True, exist_ok=True)
+    response = requests.get(download_url)
+    response.raise_for_status()
+    comicbook_title = urlsplit(download_url).path
+    filename = os.path.basename(comicbook_title)
+    with open(f'{folder}/{filename}', 'wb') as file:
+        file.write(response.content)
+    return 'ok'
 
 
 def delete_comic_book(folder):
