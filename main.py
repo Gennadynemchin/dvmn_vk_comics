b8977a79e85db86c2c3597e4d45c918d3e0272ae
gennadynemchin@gmail.com <gennadynemchin@gmail.com>
2022-10-22 13:02:40 +0300

    Edited import;

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import os
 from dotenv import load_dotenv
-from xkcd import *
-from vk import *
+from xkcd import get_comicbook, save_comicbook, delete_comic_book, get_random_cb_number
+from vk import get_upload_address, upload_photo_to_server, wall_post
 
 
 def main():
@@ -10,18 +11,15 @@ def main():
     folder = 'Files'
 
     random_comicbook = get_comicbook(get_random_cb_number(), folder)
-    try:
-        url_for_upload = get_upload_address(vk_token, vk_group_id)
-        upload = upload_photo_to_server(url_for_upload, vk_group_id, f'Files/{random_comicbook["filename"]}', vk_token)
-        group_id = f'-{vk_group_id}'
-        owner_id = upload['response'][0]['owner_id']
-        media_id = upload['response'][0]['id']
-        message = random_comicbook['comicbook_comment']
-        wall_post(group_id, owner_id, media_id, message, vk_token)
-    except KeyError:
-        print('No answer from VK server. Check your VK_USER_TOKEN')
+    url_for_upload = get_upload_address(vk_token, vk_group_id)
+    upload = upload_photo_to_server(url_for_upload, vk_group_id, f'Files/{random_comicbook["filename"]}', vk_token)
+    group_id = f'-{vk_group_id}'
+    owner_id = upload['response'][0]['owner_id']
+    media_id = upload['response'][0]['id']
+    message = random_comicbook['comicbook_comment']
+    wall_post(group_id, owner_id, media_id, message, vk_token)
+
     delete_comic_book(folder)
-    print(f'The comic book has been posted')
 
 
 if __name__ == '__main__':
