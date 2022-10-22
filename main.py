5bf776ba80bbde78383a6f124d168e7cd93f282f
gennadynemchin@gmail.com <gennadynemchin@gmail.com>
2022-10-22 19:53:39 +0300

    Added save_comicbook func();

--- main.py
+++ main.py
@@ -10,15 +10,15 @@ def main():
     vk_group_id = os.getenv('VK_GROUP_ID')
     folder = 'Files'
 
-    random_comicbook = get_comicbook(get_random_cb_number(), folder)
+    random_comicbook = get_comicbook(get_random_cb_number())
+    saved_comicbook = save_comicbook(random_comicbook['download_url'], folder)
     url_for_upload = get_upload_address(vk_token, vk_group_id)
-    upload = upload_photo_to_server(url_for_upload, vk_group_id, f'Files/{random_comicbook["filename"]}', vk_token)
+    upload = upload_photo_to_server(url_for_upload, vk_group_id, f'Files/{saved_comicbook}', vk_token)
     group_id = f'-{vk_group_id}'
     owner_id = upload['response'][0]['owner_id']
     media_id = upload['response'][0]['id']
     message = random_comicbook['comicbook_comment']
     wall_post(group_id, owner_id, media_id, message, vk_token)
-
     delete_comic_book(folder)
 
 
