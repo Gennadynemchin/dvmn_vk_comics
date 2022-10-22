7dea51406f7d5ddb83d6e66047de8a0126c111f7
gennadynemchin@gmail.com <gennadynemchin@gmail.com>
2022-10-22 19:53:14 +0300

    Added save_comicbook func();

--- xkcd.py
+++ xkcd.py
@@ -5,17 +5,12 @@ from pathlib import Path
 from urllib.parse import urlsplit
 
 
-def get_comicbook(number_of_comics, folder):
-    # Path(folder).mkdir(parents=True, exist_ok=True)
+def get_comicbook(number_of_comics):
     url = f'https://xkcd.com/{number_of_comics}/info.0.json'
     response = requests.get(url)
     response.raise_for_status()
     url_to_download_comicbook = response.json()['img']
     comicbook_comment = response.json()['alt']
-    # response_comicbook = requests.get(url_to_download_comicbook)
-    # response_comicbook.raise_for_status()
-    # with open(f'{folder}/{filename}', 'wb') as file:
-        # file.write(response_comicbook.content)
     return {'comicbook_comment': comicbook_comment,
             'download_url': url_to_download_comicbook}
 
@@ -28,7 +23,7 @@ def save_comicbook(download_url, folder):
     filename = os.path.basename(comicbook_title)
     with open(f'{folder}/{filename}', 'wb') as file:
         file.write(response.content)
-    return 'ok'
+    return filename
 
 
 def delete_comic_book(folder):
