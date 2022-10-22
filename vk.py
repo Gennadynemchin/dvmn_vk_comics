57820d500590b4a256da47d6d7e231b3632fcfc3
gennadynemchin@gmail.com <gennadynemchin@gmail.com>
2022-10-22 20:14:20 +0300

    Fixed upload_photo_to_server 'with' statement;

--- vk.py
+++ vk.py
@@ -16,20 +16,20 @@ def upload_photo_to_server(url_for_upload, group_id, photo, access_token):
     with open(photo, 'rb') as file:
         files = {'photo': file}
         response = requests.post(url_for_upload, files=files)
-        response.raise_for_status()
-        upload_to_server = response.json()
-        server = upload_to_server['server']
-        photo = upload_to_server['photo']
-        hash_photo = upload_to_server['hash']
-        url = 'https://api.vk.com/method/photos.saveWallPhoto'
-        response = requests.post(url, {'access_token': access_token,
-                                       'group_id': group_id,
-                                       'server': server,
-                                       'photo': photo,
-                                       'hash': hash_photo,
-                                       'v': '5.131'})
-        response.raise_for_status()
-        return response.json()
+    response.raise_for_status()
+    upload_to_server = response.json()
+    server = upload_to_server['server']
+    photo = upload_to_server['photo']
+    hash_photo = upload_to_server['hash']
+    url = 'https://api.vk.com/method/photos.saveWallPhoto'
+    response = requests.post(url, {'access_token': access_token,
+                                   'group_id': group_id,
+                                   'server': server,
+                                   'photo': photo,
+                                   'hash': hash_photo,
+                                   'v': '5.131'})
+    response.raise_for_status()
+    return response.json()
 
 
 def publish_wall_post(group_id, owner_id, media_id, message, access_token):
