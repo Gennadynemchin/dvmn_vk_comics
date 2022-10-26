f1885c3841285ea64359ccd13083f3c959077af7
Gennadynemchin <69091759+Gennadynemchin@users.noreply.github.com>
2022-10-26 13:19:26 +0300

    Update vk.py

--- vk.py
+++ vk.py
@@ -17,10 +17,10 @@ def upload_photo_to_server(url_for_upload, group_id, filepath, access_token):
         files = {'photo': file}
         response = requests.post(url_for_upload, files=files)
     response.raise_for_status()
-    response_vk_server = response.json()
-    server = response_vk_server['server']
-    photo = response_vk_server['photo']
-    hash_photo = response_vk_server['hash']
+    vk_server_response = response.json()
+    server = vk_server_response['server']
+    photo = vk_server_response['photo']
+    photo_hash = vk_server_response['hash']
     url = 'https://api.vk.com/method/photos.saveWallPhoto'
     response = requests.post(url, {'access_token': access_token,
                                    'group_id': group_id,
@@ -29,9 +29,9 @@ def upload_photo_to_server(url_for_upload, group_id, filepath, access_token):
                                    'hash': hash_photo,
                                    'v': '5.131'})
     response.raise_for_status()
-    response_vk_group = response.json()
-    owner_id = response_vk_group['response'][0]['owner_id']
-    media_id = response_vk_group['response'][0]['id']
+    vk_group_response = response.json()
+    owner_id = vk_group_response['response'][0]['owner_id']
+    media_id = vk_group_response['response'][0]['id']
     return owner_id, media_id
 
 
